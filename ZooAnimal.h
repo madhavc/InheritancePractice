2cf7b1db6ba45f113b7ee5e211c184076e78e47f
Madhav Chhura <chhura.madhav@gmail.com>
2014-05-27 10:46:45 -0700

    Create ZooAnimal.h

---
+++ ZooAnimal.h
@@ -0,0 +1,14 @@
+//
+//  ZooAnimal.h
+//  Pj10
+//
+//  Created by Madhav Chhura on 3/7/14.
+//  Copyright (c) 2014 Madhav Chhura. All rights reserved.
+//
+
+#ifndef __Pj10__ZooAnimal__
+#define __Pj10__ZooAnimal__
+
+#include <iostream>
+
+#endif /* defined(__Pj10__ZooAnimal__) */
